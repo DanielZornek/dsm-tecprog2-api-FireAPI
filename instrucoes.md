65bf0a65b3c45ea85a349baa653cc2e1fa3a11a7
DanielZornek <daniel_zornek91@outlook.com>
2025-03-16 20:07:24 -0300

    sistema de busca por marca e modelo pronto | a fazer: sistema por ano

--- instrucoes.md
+++ instrucoes.md
@@ -4,15 +4,9 @@ Início - https://parallelum.com.br/fipe/api/v1/carros/marcas
 
 https://fipe.parallelum.com.br/api/v1/carros/marcas/21/modelos/10392/anos/2025-1
 
-Para comprar ou vender um carro, precisamos ter uma noção do se preço, baseado na marca, no modelo e no ano. A FIPE API simula a consulta à tabela FIPE.
+Para comprar ou vender um carro, precisamos ter uma noção do preço, baseado na marca, no modelo e no ano. A FIPE API simula a consulta à tabela FIPE.
 Desafio: consumir essa API e conseguir ver o preço, baseado em um carro que você escolher.
 
 O sistema deve apresentar todas as marcas e permitir a entrada do id da marca
 O sistema deve apresentar todos os modelos e permitir a entrada do id da modelo
-O sistema deve apresentar todos os anos/serie e permitir a entrada do ano
-
-<dependency>
-    <groupId>com.google.code.gson</groupId>
-    <artifactId>gson</artifactId>
-    <version>2.12.1</version>
-</dependency>
+O sistema deve apresentar todos os anos/serie e permitir a entrada do ano       
--- src/main/java/br/edu/fatecpg/tecprog/spring/consultafireapi/ConsultaFireApiApplication.java
+++ src/main/java/br/edu/fatecpg/tecprog/spring/consultafireapi/ConsultaFireApiApplication.java
@@ -1,8 +1,12 @@
 package br.edu.fatecpg.tecprog.spring.consultafireapi;
 
+import br.edu.fatecpg.tecprog.spring.consultafireapi.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ConsultaFireApiApplication implements CommandLineRunner {
@@ -14,6 +18,80 @@ public class ConsultaFireApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Projeto Iniciado!");
+		Boolean loop_programa = true;
+		Scanner entrada = new Scanner(System.in);
+
+		while(loop_programa){
+			System.out.print("\n\nSistema de busca de Automóveis\n1- Procurar Carro\n2- Fechar\n\nEScolha: ");
+			String escolha = entrada.nextLine();
+
+			if(escolha.equals("1")){
+				listarMarcas();
+				System.out.print("Código da marca: ");
+				String codigoMarca = entrada.nextLine();
+
+				listarModelos(codigoMarca);
+				System.out.print("Código do modelo: ");
+				String codigoModelo = entrada.nextLine();
+				
+			}else if(escolha.equals("2")){
+				loop_programa = false;
+				System.out.println("Fechando programa, até mais");
+			}else{
+				System.out.println("Desculpe não entendi, tente novamente!");
+			}
+		}
+	}
+
+	protected static void listarModelos(String marca) throws IOException, InterruptedException {
+		String dados = ConsumoAPI.obterDados("https://fipe.parallelum.com.br/api/v1/carros/marcas/"+marca+"/modelos/");
+		dados = dados.replace("{", "");
+		dados = dados.replace("}", "");
+		dados = dados.replace("\"", "");
+		dados = dados.replace("[", "");
+		dados = dados.replace("]", "");
+		dados = dados.replace("modelos", "");
+		dados = dados.replace(":", "");
+
+		String[] listaCarros = dados.split(",");
+
+		for(int i = 1; i <= listaCarros.length; i+=2){
+
+			String nomeCarro = listaCarros[i];
+			nomeCarro = nomeCarro.replace("nome", "");
+			String cdCarro = listaCarros[i-1];
+			cdCarro = cdCarro.replace("codigo", "");
+
+			if(!nomeCarro.contains("Gasolina") && !nomeCarro.contains("Diesel")){
+				System.out.println("Código do carro: " + cdCarro + " | Modelo: " + nomeCarro);
+			}
+		}
+	}
+
+	protected static void listarMarcas() throws IOException, InterruptedException {
+		String dados = ConsumoAPI.obterDados("https://fipe.parallelum.com.br/api/v1/carros/marcas");
+		dados = dados.replace("[","");
+		dados = dados.replace("]","");
+		dados = dados.replace("{", "");
+
+		String[] listaMarcasSeparado = dados.split(",");
+
+		System.out.println("Marcas cadastradas: ");
+
+		for(int i = 1; i <= listaMarcasSeparado.length; i+=2){
+
+			String codigo = listaMarcasSeparado[i-1];
+			String marca = listaMarcasSeparado[i].replace("}", "");
+
+			codigo = codigo.replace("\"", "");
+			codigo = codigo.replace(":", "");
+			codigo = codigo.replace("codigo", "");
+
+			marca = marca.replace("nome", "");
+			marca = marca.replace("\"", "");
+			marca = marca.replace(":", "");
+
+			System.out.print("Codigo da marca: " + codigo + " | Marca: " + marca + "\n");
+		}
 	}
 }
